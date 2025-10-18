e3c4d71108379376cb8759e0a2d8e2e72360163c
josuemelgar151 <josuemelgar151@gmail.com>
2025-10-18 11:38:52 -0600

    ShapeTest.java

--- Caso-Estudio PU/ShapeTest.java
+++ Caso-Estudio PU/ShapeTest.java
@@ -1,20 +1,40 @@
 package service;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class ShapeTest {
+public class PointTest {
+    private Point p;
 
-    @Test
-    public void area() {
+    @Before
+    public void before() throws Exception {
+        p = new Point(1,1);
+
     }
 
     @Test
-    public void volume() {
+    public void testSetPoint() {
+        p.setPoint(1, 2);
+        String esperado = "[1.0,2.0]" ;
+        String obtenido = p.toString().toLowerCase().replaceAll("\\s+","");
+        assertEquals(esperado,obtenido);
+
     }
 
     @Test
-    public void getName() {
+    public void TestGetName() {
+        String esperado = "point" ;
+        String obtenido = p.getName().toLowerCase();
+        assertEquals(esperado,obtenido);
+    }
+
+    @Test
+    public void testToString() {
+        String esperado = "[1.0, 1.0]" ;
+        String obtenido = p.toString().toLowerCase().replaceAll("\\s+","");
+        assertEquals(esperado,obtenido);
+
     }
 }
