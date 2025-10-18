2374f1267c4b09ffe0a07cfa70bbdbea7ce18bdf
josuemelgar151 <josuemelgar151@gmail.com>
2025-10-18 11:29:49 -0600

    Add files via upload

---
+++ Caso-Estudio PU/ShapeTest.java
@@ -0,0 +1,20 @@
+package service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ShapeTest {
+
+    @Test
+    public void area() {
+    }
+
+    @Test
+    public void volume() {
+    }
+
+    @Test
+    public void getName() {
+    }
+}
